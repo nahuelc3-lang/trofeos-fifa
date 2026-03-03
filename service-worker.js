5a9ba96698c0820fc0535ed5ed2a5871f05017a9
nahuelc3-lang <nahuelc3@gmail.com>
2026-03-03 17:35:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,18 +9,14 @@ const urlsToCache = [
   "/trofeos-fifa/CONMEBOL_Sudamericana.glb"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
